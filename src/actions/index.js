84727d479bdcf992f0254e77de61c3c4f3c8dfdd
mwei2509 <m2dl89@gmail.com>
2017-04-17 17:59:52 -0400

    user info kind of works

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,29 @@
+import axios from 'axios'
+
 export const addBoardElement = (element) => {
   return {
     type: "ADD_ELEMENT",
     payload: element
+  }
+}
+
+
+//account
+export function getBoards(){
+  let config = {
+    headers: {token: window.localStorage.getItem("current user")}
+  }
+  return (dispatch) => {
+    axios
+      .get(`http://localhost:4000/account`, config)
+      .then(({data}) => {
+        dispatch({
+          type: "LOAD_USER",
+          data
+        })
+      })
+      .catch((error)=>{
+        debugger
+      })
   }
 }
